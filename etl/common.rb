81880e34e21de0719a67b902f44b2e8c7ab35fe1
Thibaut Barrère <thibaut.barrere@gmail.com>
2011-06-28 22:07:47 +0200

    Gather common stuff in the shared file.

--- etl/common.rb
+++ etl/common.rb
@@ -9,7 +9,22 @@ DATA_FOLDER = File.dirname(__FILE__) + '/../data'
 
 GIT_RAILS_REPO = File.join(DATA_FOLDER, 'git_rails')
 
+UNKNOWN_USER_NAME = 'Unknown user'
+
 # fail-fast process execution
 def system!(cmd)
   raise "Failed to run command #{cmd}" unless system(cmd)
 end
+
+# define a couple of ActiveRecord models for later re-use (not mandatory)
+
+class UserDimension < ActiveRecord::Base; end
+UserDimension.table_name = 'user_dimension'
+
+class Commit < ActiveRecord::Base
+  # TODO - rename as :author
+  belongs_to :user, :class_name => 'UserDimension'
+end
+
+# TODO - figure out why this is required here
+ActiveRecord::Base.establish_connection(:datawarehouse)
