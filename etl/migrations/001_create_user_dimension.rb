696d68aa02bf442ce9b8e29439b60a5206d9aea4
Thibaut Barrère <thibaut.barrere@gmail.com>
2012-03-03 15:59:27 +0100

    Avoid choking on "Adam" vs "adam"

--- etl/migrations/001_create_user_dimension.rb
+++ etl/migrations/001_create_user_dimension.rb
@@ -1,15 +1,15 @@
 class CreateUserDimension < ActiveRecord::Migration
 
-  def self.up
-    create_table :user_dimension do |t|
+  def change
+    create_table :user_dimension, :force => true do |t|
       t.string :name, :null => false
     end
+
+    # work-around: mysql bulk load will choke on "Adam" vs "adam", considering these are duplicates
+    # as well: we use self. because of https://github.com/activewarehouse/activewarehouse-etl/issues/70
+    self.execute %{ALTER TABLE user_dimension MODIFY name varchar(255) COLLATE utf8_bin NOT NULL}
     
     add_index :user_dimension, :name, :unique
-  end
-  
-  def self.down
-    drop_table :user_dimension
   end
   
 end
